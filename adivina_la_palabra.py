e9cc1a4d1461e0e78592766075b49802aafc6597
AnRestrepo-Z <an.restrepoz@gmail.com>
2025-01-26 00:33:05 -0500

    comprobando letra y progreso

--- adivina_la_palabra.py
+++ adivina_la_palabra.py
@@ -33,6 +33,18 @@ def adivina_la_palabra():
     while intentos >0 and "_" in progreso:
         letra = input ("\n Adivina una letra").lower
 
-       
+        if len(letra) !=1 or not letra.isalpha():
+            print("Ingresa una sola letra por portunidad")
+            continue
+        elif letra in palabra_oculta:
+            print(f"La letra {letra} está entre la palabra oculta!")
+            for i, letra_nueva in enumerate(palabra_oculta):
+                if letra_nueva == letra:
+                    progreso[i] = letra
+        else:
+            intentos -=1
+            print(f"la letra {letra} no está en la palabra. Te quedan {intentos} oportunidades")
+        
+        print("progreso: "+" ".join(progreso))
 
 
